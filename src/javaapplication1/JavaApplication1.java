d3651c3b71f62d30a99aa702507ac6de69691c47
alperenkizilkaya <36259254+alperenkizilkaya@users.noreply.github.com>
2020-06-27 22:43:07 +0300

    Update JavaApplication1.java

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -1,30 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication1;
-
 import java.util.Scanner;
 
-/**
- *
- * @author alperenkizilkaya
- */
 public class JavaApplication1 {
 
     public static void main(String[] args) {
-        System.out.println("lütfen vize notunuzu giriniz");
-        Scanner s = new Scanner(System.in);
-        float vize = s.nextFloat();
         
-        System.out.println("lütfen final notunuzu giriniz");
+        System.out.println("Please enter your midterm grade: ");
+        Scanner s = new Scanner(System.in);
+        float midterm = s.nextFloat();
+        
+        System.out.println("Please enter your final grade: ");
         float fnl = s.nextFloat();
 
-        vize = vize*40/100;
+        midterm = midterm*40/100;
         fnl = fnl*60/100;
         
-        System.out.println("yıl sonu ders ortalaması " + (vize+fnl));
-    }
-    
+        System.out.println("GPA for this course is " + (midterm+fnl));
+    } 
 }
